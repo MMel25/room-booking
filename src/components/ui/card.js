ea3659429dd97517d3d95f3bd6e5e44ca998f24a
MMel25 <maormelamed@gmail.com>
2025-02-23 00:48:41 +0200

    Update card.js

--- src/components/ui/card.js
+++ src/components/ui/card.js
@@ -1,25 +1,27 @@
 import React from 'react';
 
-export const Card = ({ children, className }) => (
-  <div className={`rounded-lg shadow-lg ${className}`}>
+const Card = ({ children, className = '', ...props }) => (
+  <div className={`bg-white rounded-lg shadow ${className}`} {...props}>
     {children}
   </div>
 );
 
-export const CardHeader = ({ children, className }) => (
-  <div className={`p-4 border-b ${className}`}>
+const CardHeader = ({ children, className = '', ...props }) => (
+  <div className={`p-4 ${className}`} {...props}>
     {children}
   </div>
 );
 
-export const CardTitle = ({ children, className }) => (
-  <div className={`text-xl font-medium ${className}`}>
+const CardTitle = ({ children, className = '', ...props }) => (
+  <h3 className={`text-lg font-semibold ${className}`} {...props}>
+    {children}
+  </h3>
+);
+
+const CardContent = ({ children, className = '', ...props }) => (
+  <div className={`p-4 pt-0 ${className}`} {...props}>
     {children}
   </div>
 );
 
-export const CardContent = ({ children, className }) => (
-  <div className={`p-4 ${className}`}>
-    {children}
-  </div>
-);
+export { Card, CardHeader, CardTitle, CardContent };
