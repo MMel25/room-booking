c9f4a587fac6e0e760225ea3782b13065f8fbd5b
MMel25 <maormelamed@gmail.com>
2025-02-22 20:54:13 +0200

    Update AccessPage.js

--- src/components/AccessPage.js
+++ src/components/AccessPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Card, CardHeader, CardTitle, CardContent } from './ui/card';
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { Lock } from 'lucide-react';
 
 const AccessPage = () => {
