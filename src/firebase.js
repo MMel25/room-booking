071dd2df0a4a1e914a6725d8cb9f15d6fdf76945
MMel25 <maormelamed@gmail.com>
2025-02-23 22:58:09 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8pd6tr5ivxM4LKpZ12XU-12DiKq7NPdE",
-  authDomain: "room-booking-a8a89.firebaseapp.com",
-  databaseURL: "https://room-booking-a8a89-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "room-booking-a8a89",
-  storageBucket: "room-booking-a8a89.firebasestorage.app",
-  messagingSenderId: "89105649436",
-  appId: "1:89105649436:web:8b80f58f97af61aa4af4d9"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
